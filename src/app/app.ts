0b7d2c850ded799ea804cedf00ce2dc391472418
si3aa <mahmoud22said22@gmail.com>
2025-07-26 13:59:29 +0300

    using ng content and do property bibnding reprtition

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerComponent } from "./dashboard/server-status/server.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [HeaderComponent, ServerComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
 })
 export class App {
 
