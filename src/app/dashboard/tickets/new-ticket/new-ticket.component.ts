1046ecfdb7cb8d1eddb06cd1957d3990c8ca90b4
si3aa <mahmoud22said22@gmail.com>
2025-08-03 16:06:57 +0300

    use viewchild tosore  form input

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/buuton/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,8 +10,13 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./new-ticket.component.css'],
 })
 export class NewTicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText);
+    //native element is an object of elementRef allow to access value from form
+    // this.form?.nativeElement.reset(); => @ViewChild.
+    this.form().nativeElement.reset();
   }
 
 }
